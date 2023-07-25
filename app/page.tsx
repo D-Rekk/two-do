d6ecc64e37c31fbbdb087cba3e1c3edba3b1d0f6
D-Rekk <andrea_derek@hotmail.it>
2023-07-25 20:57:08 +0200

    Components update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+import TopicList from "@/components/notes/topicList"
+import { Button } from "@/components/ui/button"
 import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
 import { RootLayoutProps } from "./layout"
 
 export default async function IndexPage({children} :RootLayoutProps ) {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        {children}
+        <h1 className="text-center text-2xl font-bold">Note App</h1>
+        <div className="w-full">
+          <TopicList/> 
+          <TopicList/> 
+          <TopicList/> 
+          <Link href={"/addNote"}>
+            <Button className="mt-3">New Note</Button>
+          </Link>
+        </div>
       </div>
     </section>
   )
---
+++ components/form.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  description: z.string().refine(i => i.length <= 255, {
+    message: "Description can't be longer than 255 characters"
+  })
+})
+
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      description:""
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage className="absolute"/>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage className="absolute"/>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
